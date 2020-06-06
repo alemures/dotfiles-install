dac54168764a48106c8a9a407d1303c25424af43
Alejandro Santiago <alemures@hotmail.com>
2020-06-06 20:19:25 +0200

    Fixed issue backing up dotfiles outside of $HOME

--- install.sh
+++ install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-git clone --bare https://github.com/alemures/dotfiles.git $HOME/dotfiles
+DOTFILES_FOLDER="dotfiles"
+BACKUP_FOLDER="dotfiles-backup"
+
+git clone --bare https://github.com/alemures/dotfiles.git $HOME/$DOTFILES_FOLDER
 function config {
-   /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$HOME/$DOTFILES_FOLDER/ --work-tree=$HOME $@
 }
-mkdir -p dotfiles-backup
 config checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} dotfiles-backup/{}
+else
+  echo "Backing up pre-existing dot files in $BACKUP_FOLDER.";
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs dirname | xargs -I{} mkdir -p $BACKUP_FOLDER/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUP_FOLDER/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
