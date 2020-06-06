92ab63eb3a0ea6d357b3c1234ff1d8ac4e8b077a
Alejandro Santiago <alemures@hotmail.com>
2020-06-06 20:53:04 +0200

    Added check if the dotfiles are already installed. Allowed running install.sh from any folder.

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-DOTFILES_FOLDER="dotfiles"
+DOTFILES_FOLDER="$HOME/dotfiles"
 BACKUP_FOLDER="dotfiles-backup"
 
-git clone --bare https://github.com/alemures/dotfiles.git $HOME/$DOTFILES_FOLDER
+if [ -d $DOTFILES_FOLDER ]; then
+  echo "Dotfiles already installed."
+  exit 0
+fi;
+
+git clone --bare https://github.com/alemures/dotfiles.git $DOTFILES_FOLDER
 function config {
-   /usr/bin/git --git-dir=$HOME/$DOTFILES_FOLDER/ --work-tree=$HOME $@
+   /usr/bin/git --git-dir=$DOTFILES_FOLDER/ --work-tree=$HOME $@
 }
 config checkout
 if [ $? = 0 ]; then
@@ -13,7 +18,7 @@ if [ $? = 0 ]; then
 else
   echo "Backing up pre-existing dot files in $BACKUP_FOLDER.";
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs dirname | xargs -I{} mkdir -p $BACKUP_FOLDER/{}
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $BACKUP_FOLDER/{}
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv $HOME/{} $BACKUP_FOLDER/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
