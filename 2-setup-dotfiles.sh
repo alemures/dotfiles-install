1b8c90f2ee2a7de37bc9ab365e72055f244fb455
Alejandro Santiago <alemures@hotmail.com>
2025-03-07 17:11:42 +0100

    Added packages lxappearance and lxrandr.
    - Added vscode configuration to perform autoformat.
    - Added editorconfig and prettier configuration.

--- 2-setup-dotfiles.sh
+++ 2-setup-dotfiles.sh
@@ -6,20 +6,20 @@ BACKUP_FOLDER="dotfiles-backup"
 if [ -d $DOTFILES_FOLDER ]; then
   echo "Dotfiles already installed."
   exit 0
-fi;
+fi
 
 git clone --bare https://github.com/alemures/dotfiles.git $DOTFILES_FOLDER
 function config {
-   /usr/bin/git --git-dir=$DOTFILES_FOLDER/ --work-tree=$HOME $@
+  /usr/bin/git --git-dir=$DOTFILES_FOLDER/ --work-tree=$HOME $@
 }
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Checked out config."
 else
-  echo "Backing up pre-existing dot files in $BACKUP_FOLDER.";
+  echo "Backing up pre-existing dot files in $BACKUP_FOLDER."
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs dirname | xargs -I{} mkdir -p $BACKUP_FOLDER/{}
   config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv $HOME/{} $BACKUP_FOLDER/{}
-fi;
+fi
 config checkout
 
 config config status.showUntrackedFiles no
@@ -34,4 +34,3 @@ nvm install --lts
 cd $HOME/scripts
 npm install
 npm run build
-
